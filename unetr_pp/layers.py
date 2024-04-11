125085fc9cc303ae001f3abd4f0a7c78f6dc2c94
Hongbo Zheng <hongboz2@illinois.edu>
2024-04-11 10:41:57 -0500

    update layers.py

--- unetr_pp/layers.py
+++ unetr_pp/layers.py
@@ -1,7 +1,7 @@
-import torch
-import torch.nn.functional as F
-from torch import nn
 import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class LayerNorm(nn.Module):
