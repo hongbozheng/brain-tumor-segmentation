b5ccaed7f3d682ac847eccf5307bf987d6ae9957
Hongbo Zheng <hongboz2@illinois.edu>
2024-04-12 18:24:13 -0500

    add test script

---
+++ test.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import logger
+import numpy as np
+from config import SEED, DEVICE, get_config
+from data import *
+from monai.metrics import DiceMetric
+from monai.utils.enums import MetricReduction
+from nnformer.nnFormer import nnFormer
+from swin_unetr import SwinUNETR
+from unetr import UNETR
+from unetr_pp.unetr_pp import UNETR_PP
+from unet3d import UNet3D
+from val import val_model
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="test",
+        description="inference val set with model and calculate average dice scores"
+    )
+    parser.add_argument(
+        "--model",
+        '-m',
+        type=str,
+        required=True,
+        help="model names: 'swin', 'unetr', 'unetr_pp', 'nnformer', "
+             "'unet3d'"
+    )
+
+    args = parser.parse_args()
+    model_name = args.model
+
+    model_names = {"swin", "unetr", "unetr_pp", "nnformer", "unet3d"}
+    if model_name not in model_names:
+        logger.log_error("Invalid model name.")
+        logger.log_error(f"Please choose from {model_names}")
+        exit(1)
+
+    config = get_config(args=None)
+
+    if model_name == "swin":
+        model = SwinUNETR(
+            img_size=config.MODEL.SWIN.ROI,
+            in_channels=config.MODEL.SWIN.IN_CHANNELS,
+            out_channels=config.MODEL.SWIN.OUT_CHANNELS,
+            depths=config.MODEL.SWIN.DEPTHS,
+            num_heads=config.MODEL.SWIN.NUM_HEADS,
+            feature_size=config.MODEL.SWIN.FEATURE_SIZE,
+            norm_name=config.MODEL.SWIN.NORM_NAME,
+            drop_rate=config.MODEL.SWIN.DROP_RATE,
+            attn_drop_rate=config.MODEL.SWIN.ATTN_DROP_RATE,
+            dropout_path_rate=config.MODEL.SWIN.DROPOUT_PATH_RATE,
+            normalize=config.MODEL.SWIN.NORMALIZE,
+            use_checkpoint=config.MODEL.SWIN.USE_CHECKPOINT,
+            spatial_dims=config.MODEL.SWIN.SPATIAL_DIMS,
+            downsample=config.MODEL.SWIN.DOWNSAMPLE,
+            use_v2=config.MODEL.SWIN.USE_V2,
+        )
+        ckpt_filepath = config.BEST_MODEL.SWIN
+        roi = config.MODEL.SWIN.ROI
+    elif model_name == "unetr":
+        model = UNETR(
+            in_channels=config.MODEL.UNETR.IN_CHANNELS,
+            out_channels=config.MODEL.UNETR.OUT_CHANNELS,
+            img_size=config.MODEL.UNETR.ROI,
+            feature_size=config.MODEL.UNETR.FEATURE_SIZE,
+            hidden_size=config.MODEL.UNETR.HIDDEN_SIZE,
+            mlp_dim=config.MODEL.UNETR.MLP_DIM,
+            num_heads=config.MODEL.UNETR.NUM_HEADS,
+            proj_type=config.MODEL.UNETR.PROJ_TYPE,
+            norm_name=config.MODEL.UNETR.NORM_NAME,
+            conv_block=config.MODEL.UNETR.CONV_BLOCK,
+            res_block=config.MODEL.UNETR.RES_BLOCK,
+            dropout_rate=config.MODEL.UNETR.DROPOUT_RATE,
+            spatial_dims=config.MODEL.UNETR.SPATIAL_DIMS,
+            qkv_bias=config.MODEL.UNETR.QKV_BIAS,
+            save_attn=config.MODEL.UNETR.SAVE_ATTN,
+        )
+        ckpt_filepath = config.BEST_MODEL.UNETR
+        roi = config.MODEL.UNETR.ROI
+    elif model_name == "unetr_pp":
+        model = UNETR_PP(
+            in_channels=config.MODEL.UNETR_PP.IN_CHANNELS,
+            out_channels=config.MODEL.UNETR_PP.OUT_CHANNELS,
+            feature_size=config.MODEL.UNETR_PP.FEATURE_SIZE,
+            hidden_size=config.MODEL.UNETR_PP.HIDDEN_SIZE,
+            num_heads=config.MODEL.UNETR_PP.NUM_HEADS,
+            pos_embed=config.MODEL.UNETR_PP.POS_EMBED,
+            norm_name=config.MODEL.UNETR_PP.NORM_NAME,
+            dropout_rate=config.MODEL.UNETR_PP.DROPOUT_RATE,
+            depths=config.MODEL.UNETR_PP.DEPTHS,
+            dims=config.MODEL.UNETR_PP.DIMS,
+            # conv_op=config.CONV_OP,
+            do_ds=config.MODEL.UNETR_PP.DO_DS,
+        )
+        ckpt_filepath = config.BEST_MODEL.UNETR_PP
+        roi = config.MODEL.UNETR_PP.ROI
+    elif model_name == "nnformer":
+        model = nnFormer(
+            crop_size=config.MODEL.NNFORMER.CROP_SIZE,
+            embedding_dim=config.MODEL.NNFORMER.EMBEDDING_DIM,
+            input_channels=config.MODEL.NNFORMER.INPUT_CHANNELS,
+            num_classes=config.MODEL.NNFORMER.NUM_CLASSES,
+            # conv_op=config.MODEL.NNFORMER.CONV_OP,
+            depths=config.MODEL.NNFORMER.DEPTHS,
+            num_heads=config.MODEL.NNFORMER.NUM_HEADS,
+            patch_size=config.MODEL.NNFORMER.PATCH_SIZE,
+            window_size=config.MODEL.NNFORMER.WINDOW_SIZE,
+            deep_supervision=config.MODEL.NNFORMER.DEEP_SUPERVISION,
+        )
+        ckpt_filepath = config.BEST_MODEL.NNFORMER
+        roi = config.MODEL.NNFORMER.CROP_SIZE
+    elif model_name == "unet3d":
+        model = UNet3D(
+            in_channels=config.MODEL.UNET3D.IN_CHANNELS,
+            n_class=config.MODEL.UNET3D.N_CLASS,
+            kernels=config.MODEL.UNET3D.KERNELS,
+            strides=config.MODEL.UNET3D.STRIDES,
+            norm=config.MODEL.UNET3D.NORM,
+            dim=config.MODEL.UNET3D.DIM,
+            deep_supervision=config.MODEL.UNET3D.DEEP_SUPERVISION,
+        )
+        ckpt_filepath = config.BEST_MODEL.UNET3D
+        roi = config.MODEL.UNET3D.ROI
+
+    # split data into train & val in dict format
+    train_data, val_data = train_val_split(
+        data_dir=config.DATA.DIR,
+        seed=SEED,
+        val_pct=config.DATA.VAL_PCT,
+    )
+
+    # create train & val dataloaders
+    _, val_loader = loader(
+        train_data=train_data,
+        val_data=val_data,
+        train_batch_size=config.TRAIN.BATCH_SIZE,
+        val_batch_size=config.VAL.BATCH_SIZE,
+        num_workers=config.TRAIN.NUM_WORKERS,
+        pin_memory=config.TRAIN.PIN_MEMORY,
+        roi=config.MODEL.NNFORMER.CROP_SIZE,
+    )
+
+    # acc fn (val)
+    acc_fn = DiceMetric(
+        include_background=True,
+        reduction=MetricReduction.MEAN_BATCH,
+        get_not_nans=True,
+    )
+
+    dice_scores = val_model(
+        model=model,
+        device=DEVICE,
+        ckpt_filepath=ckpt_filepath,
+        roi=roi,
+        sw_batch_size=config.VAL.SW_BATCH_SIZE,
+        overlap=config.VAL.OVERLAP,
+        acc_fn=acc_fn,
+        val_loader=val_loader,
+    )
+    avg_dice_score = np.mean(a=dice_scores, dtype=np.float32)
+
+    logger.log_info(f"TC  Dice Score: {dice_scores[0]:.6f}")
+    logger.log_info(f"WT  Dice Score: {dice_scores[1]:.6f}")
+    logger.log_info(f"ET  Dice Score: {dice_scores[2]:.6f}")
+    logger.log_info(f"Avg Dice Score: {avg_dice_score:.6f}")
+
+    return
+
+
+if __name__ == "__main__":
+    main()
