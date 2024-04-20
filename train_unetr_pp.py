753cca40c134fb11d6cfbd489fbecd22f92b5fa8
Hongbo Zheng <hongboz2@illinois.edu>
2024-04-20 13:10:52 -0500

    training unetr_pp

--- train_unetr_pp.py
+++ train_unetr_pp.py
@@ -33,14 +33,14 @@ def main() -> None:
         dataset=train_dataset,
         batch_size=config.TRAIN.BATCH_SIZE,
         shuffle=True,
-        num_workers=config.LOADER.NUM_WORKERS,
+        num_workers=config.LOADER.NUM_WORKERS_TRAIN,
         pin_memory=config.LOADER.PIN_MEMORY,
     )
     val_loader = DataLoader(
         dataset=val_dataset,
         batch_size=config.VAL.BATCH_SIZE,
         shuffle=False,
-        num_workers=config.LOADER.NUM_WORKERS,
+        num_workers=config.LOADER.NUM_WORKERS_VAL,
         pin_memory=config.LOADER.PIN_MEMORY,
     )
 
