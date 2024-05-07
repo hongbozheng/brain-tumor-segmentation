87faa0aa75149f4bbc83b7e003c76900a823ab0c
Hongbo Zheng <hongboz2@illinois.edu>
2024-05-07 17:47:03 -0500

    upload plt scripts

---
+++ plt_lr.py
@@ -0,0 +1,105 @@
+from config import get_config
+import matplotlib.pyplot as plt
+import seaborn as sns
+from lr_scheduler import LinearWarmupCosineAnnealingLR
+from torch import optim as optim
+from unetr_pp.unetr_pp import UNETR_PP
+
+
+def lr(config, model, scheduler: str) -> list[float]:
+    optimizer = optim.SGD(
+        params=model.parameters(),
+        lr=config.MODEL.UNETR_PP.LR,
+        momentum=config.MODEL.UNETR_PP.MOMENTUM,
+        weight_decay=config.MODEL.UNETR_PP.WEIGHT_DECAY,
+        nesterov=config.MODEL.UNETR_PP.NESTEROV,
+    )
+
+    if scheduler == "cos":
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            optimizer=optimizer,
+            T_max=config.TRAIN.N_EPOCHS,
+            eta_min=config.TRAIN.ETA_MIN,
+        )
+    elif scheduler == "lwcos":
+        scheduler = LinearWarmupCosineAnnealingLR(
+            optimizer=optimizer,
+            warmup_epochs=config.TRAIN.WARMUP_EPOCHS,
+            max_epochs=config.TRAIN.N_EPOCHS,
+            warmup_start_lr=config.TRAIN.WARMUP_START_LR,
+            eta_min=config.TRAIN.ETA_MIN,
+        )
+    elif scheduler == "poly":
+        scheduler = optim.lr_scheduler.PolynomialLR(
+            optimizer=optimizer,
+            total_iters=config.TRAIN.N_EPOCHS,
+            power=config.MODEL.UNETR_PP.POWER,
+        )
+
+    lrs = []
+
+    for _ in range(config.TRAIN.N_EPOCHS):
+        lrs.extend(scheduler.get_last_lr())
+        scheduler.step()
+
+    return lrs
+
+
+def plt_lr() -> None:
+    config = get_config(args=None)
+
+    model = UNETR_PP(
+        in_channels=config.MODEL.UNETR_PP.IN_CHANNELS,
+        out_channels=config.MODEL.UNETR_PP.OUT_CHANNELS,
+        feature_size=config.MODEL.UNETR_PP.FEATURE_SIZE,
+        hidden_size=config.MODEL.UNETR_PP.HIDDEN_SIZE,
+        num_heads=config.MODEL.UNETR_PP.NUM_HEADS,
+        pos_embed=config.MODEL.UNETR_PP.POS_EMBED,
+        norm_name=config.MODEL.UNETR_PP.NORM_NAME,
+        dropout_rate=config.MODEL.UNETR_PP.DROPOUT_RATE,
+        depths=config.MODEL.UNETR_PP.DEPTHS,
+        dims=config.MODEL.UNETR_PP.DIMS,
+        # conv_op=config.CONV_OP,
+        do_ds=config.MODEL.UNETR_PP.DO_DS,
+    )
+
+    cos = lr(config, model, scheduler="cos")
+    lwcos = lr(config, model, scheduler="lwcos")
+    poly = lr(config, model, scheduler="poly")
+
+    assert config.TRAIN.N_EPOCHS == len(cos) == len(lwcos) == len(poly)
+
+    plt.rc(group="font", family="serif")
+    plt.rc(group="text", usetex=True)
+
+    colors = sns.color_palette("colorblind")
+
+    fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True, figsize=(5, 4))
+
+    ax.plot(range(config.TRAIN.N_EPOCHS), cos, color=colors[0],
+            label="CosineAnnealingLR")
+    ax.plot(range(config.TRAIN.N_EPOCHS), lwcos, color=colors[6],
+            label="LinearWarmupCosineAnnealingLR")
+    ax.plot(range(config.TRAIN.N_EPOCHS), poly, color=colors[3],
+            label="PolynomialLR")
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Learning Rate")
+    ax.set_title("Learning Rate vs. Epochs")
+    ax.legend()
+
+    plt.tight_layout()
+
+    plt.savefig(f"figures/lr.png", dpi=600)
+    plt.show()
+
+    return
+
+
+def main() -> None:
+    plt_lr()
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ plt_stats.py
@@ -0,0 +1,74 @@
+import argparse
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import seaborn as sns
+
+
+def plot(filepath: str) -> None:
+    file = open(file=filepath, mode='r')
+
+    data = json.load(fp=file)
+
+    epochs = data['epoch']
+    avg_dice_loss = data['avg_dice_loss']
+    dice_score = np.array(object=data['dice_score'], dtype=np.float32)
+    avg_dice_score = data['avg_dice_score']
+
+    file.close()
+
+    assert (len(epochs) == len(avg_dice_score)
+            == dice_score.shape[0] == len(avg_dice_score))
+
+    plt.rc(group="font", family="serif")
+    plt.rc(group="text", usetex=True)
+
+    colors = sns.color_palette("colorblind")
+
+    fig, axes = plt.subplots(nrows=1, ncols=3, sharex=True, figsize=(10, 3))
+
+    axes[0].plot(epochs, avg_dice_loss)
+    axes[0].set_xlabel("Epochs")
+    axes[0].set_title("Average Dice Loss")
+
+    axes[1].plot(epochs, dice_score[:, 0], color=colors[0],
+                 label="Tumor Core")
+    axes[1].plot(epochs, dice_score[:, 1], color=colors[3],
+                 label="Whole Tumor")
+    axes[1].plot(epochs, dice_score[:, 2], color=colors[6],
+                 label="Enhancing Tumor")
+    axes[1].set_xlabel("Epochs")
+    axes[1].set_title("Dice Scores")
+    axes[1].legend()
+
+    axes[2].plot(epochs, avg_dice_score)
+    axes[2].set_xlabel("Epochs")
+    axes[2].set_title("Average Dice Score")
+
+    plt.tight_layout()
+
+    filename, _ = os.path.splitext(p=os.path.basename(p=filepath))
+    plt.savefig(f"figures/{filename}.png", dpi=600)
+    plt.show()
+
+    return
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(prog='plt', description="plot statistics")
+    parser.add_argument("--filepath",
+                        "-f",
+                        type=str,
+                        required=True,
+                        help="path to json file")
+    args = parser.parse_args()
+    filepath = args.filepath
+
+    plot(filepath=filepath)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
